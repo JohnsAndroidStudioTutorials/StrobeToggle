b88d9107b48fa9eaefc642f2556c56982c1783f1
John Sartain <jhnsartain3@gmail.com>
2017-02-11 20:23:20 -0600

    copied code from website https://www.androidtutorialpoint.com/basics/learn-by-doing/flash-light-application/

--- app/src/main/java/com/johnsandroidstudiotutorials/strobetoggle/MainActivity.java
+++ app/src/main/java/com/johnsandroidstudiotutorials/strobetoggle/MainActivity.java
@@ -1,89 +1,127 @@
 package com.johnsandroidstudiotutorials.strobetoggle;
 
+
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.pm.PackageManager;
+import android.hardware.camera2.CameraAccessException;
 import android.hardware.camera2.CameraManager;
+import android.media.MediaPlayer;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.widget.CompoundButton;
 import android.widget.ToggleButton;
 
-public class MainActivity extends AppCompatActivity {
 
-    ToggleButton strobeToggleButton;
+public class MainActivity extends AppCompatActivity {
 
     private CameraManager mCameraManager;
     private String mCameraId;
-    private boolean hasFlash;
+    private ToggleButton strobeToggleButton;
     private Boolean isTorchOn;
+    private MediaPlayer mp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d("FlashLightActivity", "onCreate()");
 
         strobeToggleButton = (ToggleButton) findViewById(R.id.strobe_toggle_button);
         isTorchOn = false;
 
-        hasFlash = getApplicationContext().getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA_FLASH);
-        if (!hasFlash) {
-            // device doesn't support flash
-            // Show alert message and close the application
+        Boolean isFlashAvailable = getApplicationContext().getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA_FLASH);
 
-            displayError();
+        if (!isFlashAvailable) {
+
+            AlertDialog alert = new AlertDialog.Builder(MainActivity.this)
+                    .create();
+            alert.setTitle("Error !!");
+            alert.setMessage("Your device doesn't support flash light!");
+            alert.setButton(DialogInterface.BUTTON_POSITIVE, "OK", new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int which) {
+                    // closing the application
+                    finish();
+                    System.exit(0);
+                }
+            });
+            alert.show();
+            return;
+        }
+
+        mCameraManager = (CameraManager) getSystemService(Context.CAMERA_SERVICE);
+        try {
+            mCameraId = mCameraManager.getCameraIdList()[0];
+        } catch (CameraAccessException e) {
+            e.printStackTrace();
         }
 
         strobeToggleButton.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                if (isTorchOn) {
-                   // turnOffFlash();
-                } else {
-                  //  turnOnFlash();
+                try {
+                    if (isTorchOn) {
+                        turnOffFlashLight();
+                        isTorchOn = false;
+                    } else {
+                        turnOnFlashLight();
+                        isTorchOn = true;
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
                 }
             }
         });
+
     }
 
-    mCameraManager = (CameraManager) getSystemService(Context.CAMERA_SERVICE);
-    try {
-        mCameraId = mCameraManager.getCameraIdList()[0];
-    } catch (CameraAccessException e) {
-        e.printStackTrace();
-    }
+    public void turnOnFlashLight() {
 
-    mTorchOnOffButton.setOnClickListener(new View.OnClickListener() {
-        @Override
-        public void onClick(View v) {
-            try {
-                if (isTorchOn) {
-                    turnOffFlashLight();
-                    isTorchOn = false;
-                } else {
-                    turnOnFlashLight();
-                    isTorchOn = true;
-                }
-            } catch (Exception e) {
-                e.printStackTrace();
+        try {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                mCameraManager.setTorchMode(mCameraId, true);
             }
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-    });
-}
+    }
 
-    private void displayError() {
-        // device doesn't support flash and/or camera.
-        // Show alert message and close the application
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setTitle("Error");
-        builder.setMessage("Sorry, your device doesn't support flash light!");
-        builder.setCancelable(false);
-        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
-            public void onClick(DialogInterface dialog, int id) {
-                finish();
+
+    public void turnOffFlashLight() {
+
+        try {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                mCameraManager.setTorchMode(mCameraId, false);
             }
-        });
-        AlertDialog alert = builder.create();
-        alert.show();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if (isTorchOn) {
+            turnOffFlashLight();
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (isTorchOn) {
+            turnOffFlashLight();
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (isTorchOn) {
+            turnOnFlashLight();
+        }
     }
 }
-
